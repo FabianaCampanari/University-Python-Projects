e70bcda01901a37ab8d01cd7d96bb5b06b077fa2
Fanbiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2023-05-14 00:43:26 -0300

    Add files via upload

---
+++ 5-Aula_5 Flag na Condicional.py
@@ -0,0 +1,10 @@
+
+idade = int(input('Digite sua idade ' ))
+maior_de_idade = idade > 18
+
+if maior_de_idade:
+  print('Você tem mais de 18 anos')
+else:
+  print('Você tem menos de 18 anos')
+
+print('fim')
