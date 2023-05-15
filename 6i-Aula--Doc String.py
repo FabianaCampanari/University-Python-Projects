f336ce608c0e0e7449893bac20bc5e911d8cce36
Fanbiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2023-05-15 02:10:03 -0300

    Add files via upload

---
+++ 6i-Aula--Doc String.py
@@ -0,0 +1,33 @@
+def soma (n1, n2):
+       ''''
+Retorna a soma de dois numeros
+
+Parametros
+----------------------------
+n1,n2:int ou float a serem semados
+
+Retorno
+------------------------------
+int ou float
+Resultado de n1 com n2
+'''
+
+       s = n1 + n2
+       return s
+
+
+
+
+
+
+
+#"funcao.py" alterado para melhor visualização do fluxo de execução.#
+
+#def soma(n1, n2):                               #1
+#print ('Início do bloco da função")  #2
+#s= n1 + n2                                           #3
+#return s                                               #4
+#print ('Fim do bloco da funcão')    #5
+
+#print ('Fora da função! ')                    ＃6
+ 
