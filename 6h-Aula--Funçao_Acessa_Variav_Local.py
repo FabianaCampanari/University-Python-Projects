ed26ee54f0f84dd2ca47c674f68568fc8323f953
Fabiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2023-06-08 18:20:32 -0300

    Delete 6h-Aula--Funçao_Acessa_Variav_Local.py

--- 6h-Aula--Funçao_Acessa_Variav_Local.py
+++
@@ -1,6 +0,0 @@
-def diga_ola(nome):#paramentros sao var locais
-        print(f'Olá {nome} !') #acessa a var local
-
-nome = 'Megan'   # variavel Global
-diga_ola('Maria')
-print(f'Tchau {nome} !')  # acessa variavel global
