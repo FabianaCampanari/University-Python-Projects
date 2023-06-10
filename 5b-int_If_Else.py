352487e90cfce3a1104ded76b8e2635317b77494
Fabiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2023-06-10 00:54:14 -0300

    5b-int_If_Else.py

---
+++ 5b-int_If_Else.py
@@ -0,0 +1,13 @@
+idade = int (input( 'digite sua idade: '))
+
+if idade > 18 :
+    print( 'Você tem mais de 18 anos')
+else:
+    print( 'Você tem menos de 18 anos ')
+    
+
+print( 'fim')    
+
+    
+    
+    
