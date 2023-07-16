ae64f187fc0d3f209101c3125fadcf18e020bf53
Fabiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2023-07-16 13:11:49 -0300

    9-for_Range_Increasing-.py

---
+++ 9-for_Range_Increasing-.py
@@ -0,0 +1,19 @@
+#LAÇO FOR (SEQUENCIAS)
+#Crie um programa que solicite ao usuario um
+#número natural e exiba a sequência crescente de
+#um até o número dado.
+
+
+n = int(input(' Type a Number:  '))
+for i in range(1, n+1): 
+    print(i, end=' ')
+    
+
+# 1- valor digitado pelo usuario
+
+# 2- O enunciado pede uma sequencia de 1 até n (1, n+1),
+#entao nao posso escrever no range (1, n) pois o range
+#é um intervalo aberto até a direta,segundo essa regra
+#se usasse range (1, n) seria asim --[1..n[--, ou seja,
+#o n ficaria de fora e nao ficaria incluso por isso uso
+#n +1 q é para incluir o valor de n--[1..n+1[
